189ffb85502e4a6862d5174be111f61fcb6071d9
Mohamed Mudassir <muthaseir@gmail.com>
2025-06-02 20:34:54 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@
 // require('dotenv').config();
 
 // const app = express();
-// app.use(cors({ origin: 'http://localhost:5173' }));
+// app.use(cors());
 // app.use(express.json());
 
 // // MongoDB Connection
